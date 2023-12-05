040c1d8ba47be2cca5eaa380a23c4d6f5f5b2e6e
Joshua Brink <joshuabrink1@gmail.com>
2023-12-05 23:07:55 +0200

    fix: add banned title and wip tags

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,18 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 });
 
 function App() {
-  const [bannedWords, setBannedWords] = useState<string[]>([]);
+  const [bannedTitles, setBannedTitles] = useState<string[]>([]);
   const [newWord, setNewWord] = useState<string>('');
+  // const [bannedTags, setBannedTags] = useState<string[]>([]);
+  // const [newTag, setNewTag] = useState<string>('');
 
   useEffect(() => {
     chrome.storage.sync.get('bannedWords', function (data) {
-      setBannedWords(data.bannedWords);
+      setBannedTitles(data.bannedWords);
     })
+    // chrome.storage.sync.get('bannedTags', function (data) {
+    //   setBannedTags(data.bannedTags);
+    // })
   }, [])
 
 
@@ -38,28 +43,30 @@ function App() {
           <Tabs defaultValue="title">
             <TabsList>
               <TabsTrigger value="title">Title</TabsTrigger>
-              <TabsTrigger value="channel">Channel</TabsTrigger>
+              <TabsTrigger value="tags">Tags</TabsTrigger>
             </TabsList>
             <TabsContent value="title">
               <ScrollArea className="rounded-md border max-h-72 overflow-y-auto" >
                 <div className="pt-4">
                   <h4 className="ml-4 mb-4 text-sm font-medium leading-none">Titles that include:</h4>
                   <Separator />
-                  {bannedWords.map((tag, i) => (
+                  {bannedTitles?.map((tag, i) => (
                     <>
-                      <div key={tag} className="ml-4 flex justify-between items-center">
-                        {tag}
+                      <div key={i} className="ml-4 flex justify-between items-center">
+                        <span className='text-base'>{tag}</span>
                         <Button variant="ghost" className='p-0 w-10' onClick={() => {
-                          chrome.storage.sync.set({ bannedWords: bannedWords.filter((word) => word !== tag) });
-                          setBannedWords(bannedWords.filter((word) => word !== tag));
+                          const updatedTitles = bannedTitles.filter((word) => word !== tag);
+                          console.log("new words", updatedTitles);
+                          chrome.storage.sync.set({ bannedWords: updatedTitles });
+                          setBannedTitles(updatedTitles);
                           chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                            chrome.tabs.sendMessage(tabs[0].id ?? 0, { data: '' });
+                            chrome.tabs.sendMessage(tabs[0].id ?? 0, { titles: updatedTitles });
                           });
                         }}>
                           <Trash size={16} />
                         </Button>
                       </div>
-                      {i !== bannedWords.length - 1 &&
+                      {i !== bannedTitles.length - 1 &&
                         <Separator />}
                     </>
                   ))}
@@ -79,10 +86,10 @@ function App() {
                 <Button
                   className='ml-2 w-12 p-0'
                   onClick={() => {
-                    const updatedWords = [...bannedWords, newWord];
+                    const updatedWords = [...bannedTitles, newWord];
 
                     // Update the state immediately to reflect the change in the UI
-                    setBannedWords(updatedWords);
+                    setBannedTitles(updatedWords);
                     setNewWord('');
 
                     // Update the storage, then send the message
@@ -91,7 +98,7 @@ function App() {
                     });
 
                     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                      chrome.tabs.sendMessage(tabs[0].id ?? 0, { data: newWord });
+                      chrome.tabs.sendMessage(tabs[0].id ?? 0, { titles: updatedWords });
                     });
                   }}
                 >
@@ -100,8 +107,65 @@ function App() {
               </div>
 
             </TabsContent>
-            <TabsContent value="channel">
-              arstars
+            <TabsContent value="tags">
+              {/* <ScrollArea className="rounded-md border max-h-72 overflow-y-auto" >
+                <div className="pt-4">
+                  <h4 className="ml-4 mb-4 text-sm font-medium leading-none">Titles that include:</h4>
+                  <Separator />
+                  {bannedTags?.map((tag, i) => (
+                    <>
+                      <div key={i} className="ml-4 flex justify-between items-center">
+                        <span className='text-base'>{tag}</span>
+                        <Button variant="ghost" className='p-0 w-10' onClick={() => {
+                          const updatedTags = bannedTags.filter((word) => word !== tag);
+                          console.log("new words", updatedTags);
+                          chrome.storage.sync.set({ bannedWords: updatedTags });
+                          setBannedTitles(updatedTags);
+                          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                            chrome.tabs.sendMessage(tabs[0].id ?? 0, { tags: updatedTags });
+                          });
+                        }}>
+                          <Trash size={16} />
+                        </Button>
+                      </div>
+                      {i !== bannedTitles.length - 1 &&
+                        <Separator />}
+                    </>
+                  ))}
+                </div>
+              </ScrollArea>
+
+              <div className="flex items-end mt-4">
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Input type="text" id="blacklist-word" placeholder="Title..." onChange={
+                    (e) => {
+                      setNewTag(e.target.value);
+                    }
+                  } value={newWord}
+                  />
+                </div>
+                <Button
+                  className='ml-2 w-12 p-0'
+                  onClick={() => {
+                    const updatedTags = [...bannedTags, newWord];
+
+                    // Update the state immediately to reflect the change in the UI
+                    setBannedTags(updatedTags);
+                    setNewTag('');
+
+                    // Update the storage, then send the message
+                    chrome.storage.sync.set({ bannedWords: updatedTags }, function () {
+                      console.log('Value is set to ' + updatedTags);
+                    });
+
+                    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                      chrome.tabs.sendMessage(tabs[0].id ?? 0, { tags: updatedTags });
+                    });
+                  }}
+                >
+                  <Plus size={18} />
+                </Button>
+              </div> */}
             </TabsContent>
           </Tabs>
 
--- src/content.js
+++ src/content.js
@@ -2,44 +2,33 @@ import { defaultBannedWords } from './banned'
 
 let bannedWords = [];
 
+// MutationObserver to detect when new thumbnails are added to the page
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations)
+    if (mutation.type === 'childList')
+      for (const node of mutation.addedNodes)
+        setBannedThumbnails(node, bannedWords)
+});
+observer.observe(document.body, { childList: true, subtree: true });
 
+
+// Listen for messages from the popup
 chrome.runtime.onMessage.addListener(function (message) {
-  if (!message.data || message.data === '') {
-    chrome.storage.sync.get('bannedWords', function (result) {
-      const wordsToUse = result.bannedWords.length > 0 ? result.bannedWords : defaultBannedWords;
-      removeThumbnail(document.body, wordsToUse);
-    });
-  } else {
-    removeThumbnail(document.body, [message.data]);
-  }
-  console.log(message.data);
+  setBannedThumbnails(document.body, message.titles);
 });
 
 
 chrome.storage.sync.get('bannedWords', function (result) {
-  if (!result.bannedWords || result.bannedWords.length === 0) {
+  if (!result.bannedWords || result.bannedWords.length === 0)
     chrome.storage.sync.set({ bannedWords: defaultBannedWords }, function () {
       console.log('Value is set to ' + defaultBannedWords);
       bannedWords = defaultBannedWords;
     });
-  } else {
-    bannedWords = result.bannedWords;
-  }
+  else bannedWords = result.bannedWords;
 });
 
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    if (mutation.type === 'childList') {
-      for (const node of mutation.addedNodes) {
-        removeThumbnail(node, bannedWords)
-      }
-    }
-  }
-});
 
-observer.observe(document.body, { childList: true, subtree: true });
-
-function removeThumbnail(node, bannedWords) {
+function setBannedThumbnails(node, bannedWords) {
   const thumbnails = node.querySelectorAll(
     ".tc-tower div, .tw-tower div"
   );
@@ -56,7 +45,7 @@ function removeThumbnail(node, bannedWords) {
         thumbnail.style.display = "none";
         console.log("banned " + titleText);
       }
-      else{
+      else {
         thumbnail.style.display = "block";
       }
     }
